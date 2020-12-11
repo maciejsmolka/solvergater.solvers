2347697551d55624998f570cbd012661b8a39bd7
Maciej Smołka (mb) <smolka@agh.edu.pl>
2020-12-11 17:00:25 +0100

    Guessing engine in heat_solver

--- R/heat_solver.R
+++ R/heat_solver.R
@@ -4,12 +4,21 @@
 #'
 #' @param wd character, working directory, the one containing
 #' `run_heat.m` and `heat_with_gradient.m`.
+#' @param engine character, computational engine, if missing or `NULL` a kind
+#' of guessing is performed.
 #' @param ... additional parameters passed to [solvergater::shell_solver()].
 #'
 #' @export
-heat_solver <- function(wd = getwd(), ...) {
+heat_solver <- function(wd = getwd(), engine = c("matlab", "octave"), ...) {
+  if (missing(engine) || is.null(engine)) {
+    engine <- guess_engine()
+  }
+  solver_cmd <- switch(engine,
+                       matlab = "matlab -nojvm -batch",
+                       octave = "octave -qfW --eval"
+                       )
   solvergater::shell_solver(
-    cmd = "matlab -nojvm -batch",
+    cmd = solver_cmd,
     nparams = 3,
     qoi_file = "qoi_value.dat",
     jacobian_file = "qoi_jacobian.dat",
@@ -19,4 +28,15 @@ heat_solver <- function(wd = getwd(), ...) {
     wd = wd,
     ...
   )
+}
+
+guess_engine <- function() {
+  if (Sys.which("matlab") != "") {
+    engine <- "matlab"
+  } else if (Sys.which("octave") != "") {
+    engine <- "octave"
+  } else {
+    stop("Engine not found", call. = FALSE)
+  }
+  engine
 }
